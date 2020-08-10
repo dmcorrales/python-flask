b02f5d01a8f412fec8ae4eefae0c43178bacc010
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-10 12:46:33 -0500

    SQL EXAMPLE

--- 1. BASES DE DATOS CON PYTHON/students.py
+++ 1. BASES DE DATOS CON PYTHON/students.py
@@ -9,6 +9,26 @@ class Student(Model):
     class Meta:
         database = db
 
+students = [
+    {'username': 'Daniel',
+    'points': 444},
+    {'username': 'Aaron',
+    'points': 555},
+    {'username': 'Lain',
+    'points': 666}
+]
+
+def add_students():
+    for student in students:
+        try:
+            Student.create(username=student['username'],
+                       points=student['points'])
+        except IntegrityError:
+            student_record = Student.get(username=student['username'])
+            student_record.points = student['points']
+            student_record.save()         
+
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Student], safe=True)
+    add_students()
