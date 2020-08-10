4cec335d8f2a32d801d4a7aa1752c12fb8d5c5b2
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-10 18:55:39 -0500

    flask finished

--- 2. FLASK/index.py
+++ 2. FLASK/index.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from flask import request 
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index(id=0):
+@app.route('/<name>')
+def index(name="Daniel"):
+    return render_template("add.html",
+    name=name)
+
+@app.route('/welcome')
+def welcome():
+    context = {"message": "hola", "title":"Welcome"}
+    return render_template("welcome.html", **context)
+
+@app.route('/example')
+def example(id=0):
     id = request.args.get('id')
     return "Current ID: {}".format(id)
 
 
 @app.route('/q')   
-@app.route('/q/<id>')
+@app.route('/q/<int:id>')
 def query(id="1"):
     id = request.args.get('id',id)
     return "Current ID: {}".format(id)
