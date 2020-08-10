7e644943d76615da9ff1d04c3637a08a4e41a027
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-10 16:39:26 -0500

    Routes

--- 2. FLASK/index.py
+++ 2. FLASK/index.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import request 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Index page"
+def index(id=0):
+    id = request.args.get('id')
+    return "Current ID: {}".format(id)
+
+
+@app.route('/q')   
+@app.route('/q/<id>')
+def query(id="1"):
+    id = request.args.get('id',id)
+    return "Current ID: {}".format(id)
+
 
 app.run(debug=True, port=8000, host='127.0.0.1')
 
