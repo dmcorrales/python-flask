b0481e38938f9d4077e71fda91ca52c1a9a9368f
Daniel Corrales <dmcorrales@unbosque.edu.co>
2020-08-10 19:44:19 -0500

    auth user models

---
+++ 3. EXAMPLE FLASK/models.py
@@ -0,0 +1,28 @@
+import datetime
+from peewee import *
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
+
+
+db = SqliteDatabase("database.sql")
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=120)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+        order_by = ('-joined_at')
+
+    @classmethod
+    def create_user(self, username, email, password):
+        try:    
+            self.create(
+                username = username,
+                email = email,
+                password = generate_password_hash(password)
+            )
+        except:
+            raise ValueError('User already exists')
